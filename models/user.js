056f0f62dc1a54217072222297af106f224c887c
John Garrett <infantrygarrett@icloud.com>
2019-04-15 22:34:52 -0500

    having issues with npm test

---
+++ models/user.js
@@ -0,0 +1,34 @@
+// bcrypt allows you to safly store user data https://www.npmjs.com/package/bcrypt
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+
+  return User;
+};
